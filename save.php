100ef91d5f2a7d4fae995a8be3494d2c0ab8cc71
PAPAJUSKUTEN <ninesztefan4@gmail.com>
2026-04-07 08:40:12 +0200

    Update save.php

--- save.php
+++ save.php
@@ -1,1 +1,12 @@
+<?php
+// Pobieranie danych z JS
+$data = json_decode(file_get_contents('php://input'), true);
 
+if ($data) {
+    // Zapis do pliku konta.json
+    file_put_contents('konta.json', json_encode($data, JSON_PRETTY_PRINT));
+    echo json_encode(["status" => "success"]);
+} else {
+    echo json_encode(["status" => "error"]);
+}
+?>
