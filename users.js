9fd53a505efca0c17a3296430080701f5b310f3f
PAPAJUSKUTEN <ninesztefan4@gmail.com>
2026-04-09 06:59:47 +0200

    Update users.js

--- users.js
+++ users.js
@@ -1,1 +1,27 @@
-
+// Baza danych Obywateli w osobnym pliku
+const citizenDatabase = [
+    {
+        user: "obywatel1",
+        pass: "start123",
+        profile: {
+            fullName: "Marek Kowalski",
+            job: "Mechanik Bennys",
+            avatar: "https://i.postimg.cc/mrxG4kRy/55c4629fb275e-o-large.webp",
+            balance: "15,500$",
+            bankAcc: "PL 0012 4432",
+            status: "Obywatel"
+        }
+    },
+    {
+        user: "karol_v3",
+        pass: "hamburg2026",
+        profile: {
+            fullName: "Karol Wiśniewski",
+            job: "LSPD - Kadet",
+            avatar: "https://i.postimg.cc/Cx1Pr8g4/IMG-2398.webp",
+            balance: "2,100$",
+            bankAcc: "PL 0099 1122",
+            status: "Obywatel"
+        }
+    }
+];
