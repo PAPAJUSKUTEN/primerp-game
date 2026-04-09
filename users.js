19790a2e1caf6d665b69ba1803b6644ec417a437
PAPAJUSKUTEN <ninesztefan4@gmail.com>
2026-04-09 07:12:36 +0200

    Update users.js

--- users.js
+++ users.js
@@ -1,4 +1,4 @@
-// Baza danych Obywateli w osobnym pliku
+// Baza danych Obywateli
 const citizenDatabase = [
     {
         user: "obywatel1",
@@ -9,7 +9,7 @@ const citizenDatabase = [
             avatar: "https://i.postimg.cc/mrxG4kRy/55c4629fb275e-o-large.webp",
             balance: "15,500$",
             bankAcc: "PL 0012 4432",
-            status: "Obywatel"
+            status: "OBYWATEL-01"
         }
     },
     {
@@ -21,7 +21,7 @@ const citizenDatabase = [
             avatar: "https://i.postimg.cc/Cx1Pr8g4/IMG-2398.webp",
             balance: "2,100$",
             bankAcc: "PL 0099 1122",
-            status: "Obywatel"
+            status: "OBYWATEL-02"
         }
     }
 ];
