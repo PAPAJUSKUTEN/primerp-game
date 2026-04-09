cece7cab8b84764b3f590f89b330d1fa18639847
PAPAJUSKUTEN <ninesztefan4@gmail.com>
2026-04-09 09:21:56 +0200

    Update users.js

--- users.js
+++ users.js
@@ -1,14 +1,43 @@
+/**
+ * BAZA DANYCH OBYWATELI PRIME RP
+ * Tutaj możesz dodawać nieskończoną liczbę kont.
+ * Każdy obiekt musi mieć: user, pass oraz profile.
+ */
 const citizenDatabase = [
     {
-        user: "gracz1",
-        pass: "haslo123",
+        user: "kowalski",
+        pass: "prime123",
         profile: {
-            fullName: "Jan Kowalski",
-            avatar: "https://link-do-zdjecia.pl",
-            job: "Bezrobotny",
+            fullName: "Janusz Kowalski",
+            avatar: "https://i.postimg.cc/8519krYf/270aac72-dcb0-4b08-bca6-44c6da70ee5f-profile-image-300x300.webp",
+            job: "Kierowca Taxi",
             status: "OBYWATEL",
-            balance: "100$",
-            bankAcc: "12345"
+            balance: "12,450 $",
+            bankAcc: "PL 40 2000 1001"
         }
-    } // Sprawdź czy tutaj jest zamknięcie klamry i nawiasu!
+    },
+    {
+        user: "nowak",
+        pass: "haslo",
+        profile: {
+            fullName: "Adam Nowak",
+            avatar: "https://i.postimg.cc/mrxG4kRy/55c4629fb275e-o-large.webp",
+            job: "Mechanik LSC",
+            status: "ZAUFANY OBYWATEL",
+            balance: "85,000 $",
+            bankAcc: "PL 11 9999 0002"
+        }
+    },
+    {
+        user: "medyk1",
+        pass: "pomoc",
+        profile: {
+            fullName: "Katarzyna Leczy",
+            avatar: "https://i.postimg.cc/0Nz4jwXJ/Zrzut-ekranu-2024-03-08-195813.webp",
+            job: "Lekarz Dyżurny",
+            status: "SŁUŻBY MEDYCZNE",
+            balance: "4,200 $",
+            bankAcc: "PL 05 3333 4444"
+        }
+    }
 ];
