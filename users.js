d2bf279b3a544b4fa1765974cb2dda83c949cd9c
PAPAJUSKUTEN <ninesztefan4@gmail.com>
2026-04-09 09:18:26 +0200

    Update users.js

--- users.js
+++ users.js
@@ -1,39 +1,14 @@
-// Baza danych obywateli Prime RP
 const citizenDatabase = [
     {
-        user: "gracz1", // Login do wpisania w Autoryzacji
-        pass: "haslo123", // Hasło do wpisania w Autoryzacji
+        user: "gracz1",
+        pass: "haslo123",
         profile: {
-            fullName: "Janusz Pistolet",
-            avatar: "https://i.postimg.cc/8519krYf/270aac72-dcb0-4b08-bca6-44c6da70ee5f-profile-image-300x300.webp",
+            fullName: "Jan Kowalski",
+            avatar: "https://link-do-zdjecia.pl",
             job: "Bezrobotny",
             status: "OBYWATEL",
-            balance: "1,500 $",
-            bankAcc: "PL 00 1234 5678"
+            balance: "100$",
+            bankAcc: "12345"
         }
-    },
-    {
-        user: "komendant",
-        pass: "policja2026",
-        profile: {
-            fullName: "Andrzej Szeryf",
-            avatar: "https://i.postimg.cc/mrxG4kRy/55c4629fb275e-o-large.webp",
-            job: "Komendant LSPD",
-            status: "FUNKCJONARIUSZ",
-            balance: "45,000 $",
-            bankAcc: "PL 99 8888 1111"
-        }
-    },
-    {
-        user: "medyk",
-        pass: "lsms123",
-        profile: {
-            fullName: "Anna Leczy",
-            avatar: "https://i.postimg.cc/0Nz4jwXJ/Zrzut-ekranu-2024-03-08-195813.webp",
-            job: "Ordynator LSMS",
-            status: "ZAUFANY OBYWATEL",
-            balance: "12,300 $",
-            bankAcc: "PL 11 2222 3333"
-        }
-    }
+    } // Sprawdź czy tutaj jest zamknięcie klamry i nawiasu!
 ];
