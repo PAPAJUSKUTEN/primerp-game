eac083032050b951ce2d2d87dd55ea87697233a1
PAPAJUSKUTEN <ninesztefan4@gmail.com>
2026-06-17 18:02:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import os
 
+def run_vps_site():
+    # Pobiera port od Rendera, a jeśli go nie ma (lokalnie), daje 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
